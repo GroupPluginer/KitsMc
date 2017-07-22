c1f25de6d46ecb78e31752a97dbbe9290dfe1428
GroupPluginer <4gamesl7o@gmail.com>
2017-07-22 05:26:09 +0300

    Update Main.php

--- src/Kits/Main.php
+++ src/Kits/Main.php
@@ -20,14 +20,14 @@ use pocketmine\player;
      public function onCommand(CommandSender $sender, Command $cmd, $lable, array $args){
       switch($cmd->getName()){
        case 'vip':
-        $player->getInventory()->setHelmet(item::get(310, 0, 1));
-        $player->getInventory()->setChestPlate(item::get(311, 0, 1));
-        $player->getInventory()->setBoost(item::get(313, 0, 1));
-        $player->getInventory()->setLeggings(item::get(312, 0, 1));
-        $player->getInventory()->addItem(item::get(272, 0, 1));
-        $player->getInventory()->addItem(item::get(322, 0, 15));
-        $player->getInventory()->addItem(item::get(261, 0, 1));
-        $player->getInventory()->addItem(item::get(262, 0, 15));
+        $player->getInventory()->setHelmet(Item::get(310, 0, 1));
+        $player->getInventory()->setChestPlate(Item::get(311, 0, 1));
+        $player->getInventory()->setBoost(Item::get(313, 0, 1));
+        $player->getInventory()->setLeggings(Item::get(312, 0, 1));
+        $player->getInventory()->addItem(Item::get(272, 0, 1));
+        $player->getInventory()->addItem(Item::get(322, 0, 15));
+        $player->getInventory()->addItem(Item::get(261, 0, 1));
+        $player->getInventory()->addItem(Item::get(262, 0, 15));
         $player->sendMessage("Done you get KIT vip");
         break;
       }
